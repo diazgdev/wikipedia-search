c183f3a7fa0383b5ce124bd6c0410fe6ae9d0272
Guillermo Diaz <diazgdev@gmail.com>
2022-05-15 23:41:54 -0500

    now the search results show below the search bar

---
+++ main.js
@@ -0,0 +1,51 @@
+let form = document.querySelector('.search-form');
+
+form.addEventListener('submit', handleSubmit);
+
+async function handleSubmit(event) {
+  // prevent page from reloading when form is submitted
+  event.preventDefault();
+  // get the value from the input field
+  let inputValue = document.querySelector('.search-input').value;
+  // remove whitespaces in the search query
+  let searchQuery = inputValue.trim();
+
+  try {
+    let results = await searchWikipedia(searchQuery);
+    console.log(results);
+    displayResults(results);
+  } catch (error) {
+    console.log(error);
+    alert('Failed to search in Wikipedia');
+  }
+}
+
+async function searchWikipedia(searchQuery) {
+  let endpoint = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${searchQuery}`;
+  let response = await fetch(endpoint);
+  if (!response.ok) {
+    throw Error(response.statusText);
+  }
+  let json = await response.json();
+  return json;
+}
+
+function displayResults(results) {
+  let searchResults = document.querySelector('.results');
+
+  results.query.search.forEach(result => {
+    let url = `https://en.wikipedia.org/?curid=${result.pageid}`;
+
+      // Append the search result to the DOM
+      searchResults.insertAdjacentHTML(
+        'beforeend',
+        `<div class="result-item">
+          <h3 class="result-title">
+            <a href="${url}" target="_blank" rel="noopener">${result.title}</a>
+          </h3>
+          <a href="${url}" class="result-link" target="_blank" rel="noopener">
+          <span class="result-snippet">${result.snippet}</span><br>
+          </div>`
+     );
+  });
+}
